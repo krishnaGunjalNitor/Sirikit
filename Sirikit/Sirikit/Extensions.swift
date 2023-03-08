d300b3fea7541b9a2900b3b8e7154397cf4ced6f
Krishna Gunjal <krishna.gunjal@nitorinfotech.com>
2023-03-08 13:20:32 +0530

    Added functionality to add commant to use with sirikit.

---
+++ Sirikit/Sirikit/Extensions.swift
@@ -0,0 +1,67 @@
+//
+//  Extensions.swift
+//  Sirikit
+//
+//  Created by Krishna Gunjal on 08/03/23.
+//
+
+import Foundation
+import IntentsUI
+
+extension ViewController {
+    @available(iOS 12.0, *)
+    public var intent: INPlayMediaIntent {
+        let testIntent = INPlayMediaIntent()
+        testIntent.suggestedInvocationPhrase = "nitor"
+        return testIntent
+    }
+}
+
+extension ViewController: INUIAddVoiceShortcutButtonDelegate {
+    @available(iOS 12.0, *)
+    func present(_ addVoiceShortcutViewController: INUIAddVoiceShortcutViewController, for addVoiceShortcutButton: INUIAddVoiceShortcutButton) {
+        addVoiceShortcutViewController.delegate = self
+        addVoiceShortcutViewController.modalPresentationStyle = .formSheet
+        present(addVoiceShortcutViewController, animated: true, completion: nil)
+    }
+    
+    @available(iOS 12.0, *)
+    func present(_ editVoiceShortcutViewController: INUIEditVoiceShortcutViewController, for addVoiceShortcutButton: INUIAddVoiceShortcutButton) {
+        editVoiceShortcutViewController.delegate = self
+        editVoiceShortcutViewController.modalPresentationStyle = .formSheet
+        present(editVoiceShortcutViewController, animated: true, completion: nil)
+    }
+    
+    
+}
+
+extension ViewController: INUIAddVoiceShortcutViewControllerDelegate {
+    @available(iOS 12.0, *)
+    func addVoiceShortcutViewController(_ controller: INUIAddVoiceShortcutViewController, didFinishWith voiceShortcut: INVoiceShortcut?, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    @available(iOS 12.0, *)
+    func addVoiceShortcutViewControllerDidCancel(_ controller: INUIAddVoiceShortcutViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
+
+extension ViewController: INUIEditVoiceShortcutViewControllerDelegate {
+    @available(iOS 12.0, *)
+    func editVoiceShortcutViewController(_ controller: INUIEditVoiceShortcutViewController, didUpdate voiceShortcut: INVoiceShortcut?, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    @available(iOS 12.0, *)
+    func editVoiceShortcutViewController(_ controller: INUIEditVoiceShortcutViewController, didDeleteVoiceShortcutWithIdentifier deletedVoiceShortcutIdentifier: UUID) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    @available(iOS 12.0, *)
+    func editVoiceShortcutViewControllerDidCancel(_ controller: INUIEditVoiceShortcutViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
--- Sirikit/Sirikit/ViewController.swift
+++ Sirikit/Sirikit/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import IntentsUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.addSiriButton(to: self.view)
     }
 
-
+    func addSiriButton(to view: UIView) {
+     if #available(iOS 12.0, *) {
+         let button = INUIAddVoiceShortcutButton(style: .whiteOutline)
+             button.shortcut = INShortcut(intent: intent )
+             button.delegate = self
+             button.translatesAutoresizingMaskIntoConstraints = false
+             view.addSubview(button)
+             view.centerXAnchor.constraint(equalTo: button.centerXAnchor).isActive = true
+             view.centerYAnchor.constraint(equalTo: button.centerYAnchor).isActive = true
+         }
+     }
+     
+     func showMessage() {
+         let alert = UIAlertController(title: "Done!", message: "This is your first shortcut action!", preferredStyle: UIAlertController.Style.alert)
+         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+         self.present(alert, animated: true, completion: nil)
+     }
 }
 
